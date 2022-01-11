7770522ca5361476d71ab5fdafef4891c2273f08
buaaymh <yangminghao@buaa.edu.cn>
2022-01-11 19:46:42 +0800

    Add Eigen test.

---
+++ applications/test/Eigen/Test-Eigen.C
@@ -0,0 +1,53 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) 2011-2019 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    Test-Eigen
+
+Description
+    This is the OpenFOAM Eigen test from buaaymh.
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include <iostream>
+#include <Eigen/Dense>
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+    Eigen::MatrixXd m(2,2);
+    m(0,0) = 3;
+    m(1,0) = 2.5;
+    m(0,1) = -1;
+    m(1,1) = m(1,0) + m(0,1);
+    std::cout << "Here is the matrix m:\n" << m << std::endl;
+    Eigen::VectorXd v(2);
+    v(0) = 4;
+    v(1) = v(0) - 1;
+    std::cout << "Here is the vector v:\n" << v << std::endl;
+    return 0;
+}
+
+// ************************************************************************* //
