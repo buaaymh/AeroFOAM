bf6b3c0e2d1f2e22f946fe1497fd6486fd868f74
buaaymh <yangminghao@buaa.edu.cn>
2022-03-05 13:31:04 +0800

    Eigen cast function test.

--- applications/test/Eigen/Test-Eigen.C
+++ applications/test/Eigen/Test-Eigen.C
@@ -37,16 +37,11 @@ Description
 
 int main(int argc, char *argv[])
 {
-    Eigen::MatrixXd m(2,2);
-    m(0,0) = 3;
-    m(1,0) = 2.5;
-    m(0,1) = -1;
-    m(1,1) = m(1,0) + m(0,1);
-    std::cout << "Here is the matrix m:\n" << m << std::endl;
-    Eigen::VectorXd v(2);
-    v(0) = 4;
-    v(1) = v(0) - 1;
-    std::cout << "Here is the vector v:\n" << v << std::endl;
+    Eigen::Matrix<double, 2, 2> mat;
+    mat << 2, 0,
+           0, 2;
+    Eigen::Matrix<float, 2, 2> mat_1 = mat.inverse().cast<float>();
+    std::cout << mat_1 << std::endl;
     return 0;
 }
 
